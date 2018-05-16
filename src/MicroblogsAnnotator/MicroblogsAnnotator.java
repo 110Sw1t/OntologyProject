f1d39b4334523fbc67c2873cc5646d1f23d9f502
Ahmed1337 <ahmedhamed523@gmail.com>
2018-05-16 23:06:17 +0200

    .

--- src/MicroblogsAnnotator/MicroblogsAnnotator.java
+++ src/MicroblogsAnnotator/MicroblogsAnnotator.java
@@ -7,6 +7,8 @@ package MicroblogsAnnotator;
 
 import java.util.Arrays;
 
+
+
 /**
  *
  * @author khaled
@@ -17,14 +19,14 @@ public class MicroblogsAnnotator {
         //add relative path here, will do it later
         String inputPath = "tweets.txt";
         String OutputPath = "keywords.txt";
-        //Runtime.getRuntime().exec("python First-Block.py").waitFor();
-        //Runtime.getRuntime().exec("python generate_candidates.py").waitFor();
+        Runtime.getRuntime().exec("python First-Block.py").waitFor();
+        Runtime.getRuntime().exec("python generate_candidates.py").waitFor();
         //Runtime.getRuntime().exec("getTweetsTimeStamps.py").waitFor(); //commented because the free api allows us to get only the tweets of the last 7 days,
                                                                          //and we managed to gather data for 2 weeks, by running it every 2 days
         GateTool g = new GateTool(inputPath, OutputPath);
         g.RunTwitIE();
         //g.RunANNIE(); 
-        //System.out.println(Arrays.toString(OntologiesContextSimilarity.getContextandTimingSimilarities("Avengers", OntologyTimeSimilarity.getTimingSimilarity())));
+        System.out.println(Arrays.toString(OntologiesContextSimilarity.getContextandTimingSimilarities("Avengers", OntologyTimeSimilarity.getTimingSimilarity())));
     }
 
 }
